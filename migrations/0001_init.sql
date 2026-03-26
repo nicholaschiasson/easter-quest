91a964171af6da4bfdbe45c3e61602f5c38da680
nchiasson <nicholas.chiasson@ubisoft.com>
2026-03-26 12:46:12 -0400

    Fixing 'works on my machine' and hardcoding egg ids

--- migrations/0001_init.sql
+++ migrations/0001_init.sql
@@ -20,25 +20,25 @@ CREATE TABLE IF NOT EXISTS user_eggs (
 );
 
 -- Bed
-INSERT INTO eggs (title, content_uri) VALUES ('Achievement Unlocked!', 'egg01.html');
+INSERT INTO eggs (id, title, content_uri) VALUES ('85160ada-30fd-4fc4-979c-008a10a18ff5', 'Achievement Unlocked!', 'egg01.html');
 
 -- Bowl
-INSERT INTO eggs (title, content_uri) VALUES ('Breakfast of Champions', 'egg02.html');
+INSERT INTO eggs (id, title, content_uri) VALUES ('72ed3d00-56d6-46eb-ac32-40c0c24ed6b7', 'Breakfast of Champions', 'egg02.html');
 
 -- Fortitude
-INSERT INTO eggs (title, content_uri) VALUES ('Caffeine Buzzkiller', 'egg03.html');
+INSERT INTO eggs (id, title, content_uri) VALUES ('8011d3b1-b357-4f25-9272-795cca1086a4', 'Caffeine Buzzkiller', 'egg03.html');
 
 -- Bike
-INSERT INTO eggs (title, content_uri) VALUES ('The Wheels on the Bike Go Round and Round', 'egg04.html');
+INSERT INTO eggs (id, title, content_uri) VALUES ('d3aacfed-cb66-433b-aa8d-1d7c938f6cd0', 'The Wheels on the Bike Go Round and Round', 'egg04.html');
 
 -- Parakeets
-INSERT INTO eggs (title, content_uri) VALUES ('Don''t Bite the Hand that Feeds You', 'egg05.html');
+INSERT INTO eggs (id, title, content_uri) VALUES ('538d041c-8a3a-461b-8a1e-939ff512e2a8', 'Don''t Bite the Hand that Feeds You', 'egg05.html');
 
 -- Clock tower
-INSERT INTO eggs (title, content_uri) VALUES ('Sir Benjamin the Big', 'egg06.html');
+INSERT INTO eggs (id, title, content_uri) VALUES ('218c50bd-b436-4153-99a5-4f84363e1dbd', 'Sir Benjamin the Big', 'egg06.html');
 
 -- Pub
-INSERT INTO eggs (title, content_uri) VALUES ('Three Cheers for Friendship!', 'egg07.html');
+INSERT INTO eggs (id, title, content_uri) VALUES ('e43c8174-d252-4ac7-8f39-c420d1584390', 'Three Cheers for Friendship!', 'egg07.html');
 
 -- Under bed
-INSERT INTO eggs (title, content_uri) VALUES ('CONGRATULATIONS!', 'egg08.html');
+INSERT INTO eggs (id, title, content_uri) VALUES ('fb0ae7f9-6a51-4478-82fa-0860e0f1d0aa', 'CONGRATULATIONS!', 'egg08.html');
